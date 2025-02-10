3e09791235c81f49c203264be89f86c2820ced1a
MLGalusha <masonlgalusha@gmail.com>
2025-02-10 16:37:33 -0500

    initial commit adding envelope with opening animation

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="contain-main-page">
+      <div className="contain-envelope">
+        <div className="envelope">
+          <div className="envelope-closed"></div>
+          <div className="envelope-letter"></div>
+          <div className="envelope-opened-top"></div>
+          <div className="envelope-opened"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
