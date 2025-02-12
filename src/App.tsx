b85317335068ae06a140804554732ea6fbddd4b0
MLGalusha <masonlgalusha@gmail.com>
2025-02-12 02:47:32 -0500

    added SweetheartsBackground to App, updated view height, and default colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 import Letter from "./components/Letter";
+import NoPage from "./components/NoPage";
+import SweetheartsBackground from "./components/SweetheartsBackground";
 
 function App() {
   const [currentPage, setCurrentPage] = useState<string>("start");
-  // Pages [start, letter, no, yes]
+
+  function updatePage(newPage: string) {
+    setCurrentPage(newPage);
+  }
 
   return (
     <div className="contain-main-page">
+      <SweetheartsBackground />
       {currentPage === "start" ? (
-        <div
-          className="contain-envelope"
-          onClick={() => setCurrentPage("letter")}
-        >
+        <div className="contain-envelope" onClick={() => updatePage("letter")}>
+          <div className="env-shadow" />
           <div className="envelope">
             <div className="envelope-closed"></div>
             {/* <div className="envelope-letter"></div> */}
             <div className="contain-letter">
-              <Letter onSetCurrentPage={setCurrentPage} isFullScreen={false} />
+              <Letter onSetCurrentPage={updatePage} isFullScreen={false} />
             </div>
             <div className="envelope-opened-top"></div>
             <div className="envelope-opened"></div>
@@ -25,18 +29,21 @@ function App() {
         </div>
       ) : currentPage === "letter" ? (
         <div>
-          <Letter onSetCurrentPage={setCurrentPage} isFullScreen={true} />
+          <Letter onSetCurrentPage={updatePage} isFullScreen={true} />
         </div>
       ) : currentPage === "no" ? (
         <div>
-          <h3 onClick={() => setCurrentPage("letter")}>
-            Need to think about your decision again?
-          </h3>
-          <h1>NOOOOOOOO!!!!</h1>
+          <NoPage onSubmit={() => setCurrentPage("final-no-page")} />
+        </div>
+      ) : currentPage === "final-no-page" ? (
+        <div>
+          <h1>
+            Wow what a loser you just wasted your time on that dumbass form!
+          </h1>
         </div>
       ) : (
         <div>
-          <h3 onClick={() => setCurrentPage("letter")}>
+          <h3 onClick={() => updatePage("letter")}>
             You didn't mean to select yes?
           </h3>
           <h1>YESSSSSSSSS!!!!</h1>
